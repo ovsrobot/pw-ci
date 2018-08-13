bf7b021fd16cdb4b28559d280e804610529c1c38
Aaron Conole <aconole@redhat.com>
2018-08-13 15:35:47 -0400

    pwmon: account for incomplete series
    Signed-off-by: Aaron Conole <aconole@redhat.com>

--- series_db_lib.sh
+++ series_db_lib.sh
@@ -15,6 +15,27 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+function run_db_command() {
+    echo "$@" | sqlite3 ${HOME}/.series-db 2>/dev/null
+}
+
+function series_db_upgrade() {
+    # 0000 - upgrade infrastructure
+    run_db_command "select * from series_schema_version;" >/dev/null
+    if [ $? -eq 1 ]; then
+        run_db_command "CREATE TABLE series_schema_version (id INTEGER);"
+        run_db_command "INSERT INTO series_schema_version(id) values (0);"
+    fi
+
+    run_db_command "select * from series_schema_version;" | egrep '^1$' >/dev/null 2>&1
+    if [ $? -eq 1 ]; then
+        sqlite3 ${HOME}/.series-db <<EOF
+ALTER TABLE series ADD COLUMN series_completed INTEGER;
+EOF
+        run_db_command "INSERT INTO series_schema_version(id) values (1);"
+    fi
+}
+
 function series_db_exists() {
     if [ ! -e ${HOME}/.series-db ]; then
         sqlite3 ${HOME}/.series-db <<EOF
@@ -28,6 +49,7 @@ series_submitted BOOLEAN
 );
 EOF
     fi
+    series_db_upgrade
 }
 
 function series_db_execute() {
@@ -37,7 +59,7 @@ function series_db_execute() {
             NOTDONE="true"
             series_db_exists
         fi
-        echo "$command" | sqlite3 ${HOME}/.series-db 2>/dev/null
+        run_db_command "$command"
     done
 }
 
@@ -47,8 +69,9 @@ function series_db_add_false() {
     url="$3"
     submitter_name="$4"
     submitter_email="$5"
+    completed="$6"
 
-    echo "insert into series(series_id, series_project, series_url, series_submitter, series_email, series_submitted) values (${id}, \"${project}\", \"${url}\", \"${submitter_name}\", \"${submitter_email}\", \"false\");" | series_db_execute
+    echo "insert into series(series_id, series_project, series_url, series_submitter, series_email, series_submitted, series_completed) values (${id}, \"${project}\", \"${url}\", \"${submitter_name}\", \"${submitter_email}\", \"false\", \"${completed}\");" | series_db_execute
 }
 
 function series_id_exists() {
@@ -69,7 +92,15 @@ function get_unsubmitted_jobs_as_line() {
 
     series_db_exists
 
-    echo "select series_id,series_url,series_submitter,series_email from series where series_project=\"$project\" and series_submitted=\"false\";" | series_db_execute
+    echo "select series_id,series_url,series_submitter,series_email from series where series_project=\"$project\" and series_completed=\"1\" and series_submitted=\"false\";" | series_db_execute
+}
+
+function get_uncompleted_jobs_as_line() {
+    project="$1"
+
+    series_db_exists
+
+    echo "select series_id,series_url,series_submitter,series_email from series where series_project=\"$project\" and series_completed=\"0\" and series_submitted=\"false\";" | series_db_execute
 }
 
 function series_id_set_submitted() {
@@ -81,4 +112,14 @@ function series_id_set_submitted() {
     
     echo "update series set series_submitted=\"true\" where series_id=$id;" | series_db_execute
     return 0
+}
+
+function series_id_set_complete() {
+    id="$1"
+
+    if ! series_id_exists "$id"; then
+        return 0
+    fi
+
+    echo "update series set series_complete=\"1\" where series_id=$id;" | series_db_execute
 }
