ovsrobot/pw-ci

commit ff9efbc1c662094811de94de648400fe364e694c

pw_mon: clear poll flag for old series in the DB
Series will possibly pile up if the builds associated were
interrupted or otherwise altered.  This has the disadvantage
of causing the system to continually poll for build status
even on builds that were intentionally cancelled.  If a
patch is superseded, rejected, or accepted, we don't
even need to care any longer.

Signed-off-by: Aaron Conole <aconole@redhat.com>

pw_mon
@@ -135,6 +135,21 @@ function check_undownloaded_series() {
     return 0
 }
 
+function check_superseded_series() {
+    local pw_instance="$1"
+    series_get_active_branches "$pw_instance" | while IFS=\| read -r series_id project url repo branchname; do
+        # first query the patch states
+        local last_patch_url=$(curl -s "$userpw" "$url" | jq -rc '.patches[-1].url')
+        local patch_state=$(curl -s "$userpw" "$last_patch_url" | jq -rc '.state')
+
+        # now check to see if the patch should even be reported...
+        if [ "$patch_state" = "superseded" -o "$patch_state" = "rejected" -o "$patch_state" = "accepted" ]; then
+            series_clear_branch "$pw_instance" "$series_id"
+        fi
+    done
+}
+
 check_undownloaded_series "$pw_instance" "$pw_project"
 check_completed_series "$pw_instance" "$pw_project"
 check_new_series "$pw_instance" "$pw_project"
+check_superseded_series "$pw_instance"

commit bfad8cae36a1d4c579f4c84aeb588395a9c6e47c

pw_mon: set sync flag for old series in the DB for git table
This is a continuation of the work done in the commit refernced below,
but this time is for github builds

Fixes: ff9efbc ("pw_mon: clear poll flag for old series in the DB")
Signed-off-by: Michael Santana <msantana@redhat.com>
Signed-off-by: Aaron Conole <aconole@redhat.com>

pw_mon
@@ -148,6 +148,7 @@ function check_superseded_series() {
         if [ "$patch_state" = "superseded" -o "$patch_state" = "rejected" -o "$patch_state" = "accepted" \
              -o "$patch_state" = "changes-requested" -o "$patch_state" = "not-applicable" ]; then
             series_clear_branch "$pw_instance" "$series_id"
+            set_synced_for_series "$series_id" "$pw_instance"
         fi
         echo "Worked on $series_id : state $patch_state"
     done

series_db_lib.sh
@@ -344,6 +344,15 @@ function set_synced_patch() {
     echo "update git_builds set gap_sync=1 where patchwork_instance=\"$instance\" and patch_id=$patch_id;" | series_db_execute
 }
 
+function set_synced_for_series() {
+    local series_id="$1"
+    local instance="$2"
+
+    series_db_exists
+
+    echo "update git_builds set gap_sync=1 where patchwork_instance=\"$instance\" and series_id=$series_id;" | series_db_execute
+}
+
 function insert_commit() {
     local series_id="$1"
     local patch_id="$2"