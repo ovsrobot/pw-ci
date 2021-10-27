ovsrobot/pw-ci

commit e16b2ffea1d27277f9116e61fa87d17bd965d684

pw-ci: allow for downloads to fail
Periodically, a patchwork instance might receive enough traffic that a
URL get fails.  This happens occasionally with a running instance of
the 0-day bot.  When it occurs, we need to re-submit the series.

This allows that to happen in a way that will continuously re-try without
blocking forward progress by constantly allowing a complete batch to
proceed.

Acked-by: Michael Santana <msantana@redhat.com>
Signed-off-by: Aaron Conole <aconole@redhat.com>

series_db_lib.sh
@@ -49,6 +49,15 @@ EOF
         fi
         run_db_command "INSERT INTO series_schema_version(id) values (2);"
     fi
+
+    # 0003 - series download retry mechanism
+    run_db_command "select * from series_schema_version;" | egrep '^3$' >/dev/null 2>&1
+    if [ $? -eq 1 ]; then
+        sqlite3 ${HOME}/.series-db <<EOF
+ALTER TABLE series ADD COLUMN series_downloaded INTEGER;
+EOF
+        run_db_command "INSERT INTO series_schema_version(id) values (3);"
+    fi
 }
 
 function series_db_exists() {
@@ -121,6 +130,15 @@ function get_uncompleted_jobs_as_line() {
     echo "select series_id,series_url,series_submitter,series_email from series where series_instance=\"$instance\" and series_project=\"$project\" and series_completed=\"0\" and series_submitted=\"false\" and series_downloaded=\"0\";" | series_db_execute
 }
 
+function get_undownloaded_jobs_as_line() {
+    local instance="$1"
+    local project="$2"
+
+    series_db_exists
+
+    echo "select series_id,series_url,series_submitter,series_email from series where series_instance=\"$instance\" and series_project=\"$project\" and series_completed=\"1\" and series_submitted=\"true\" and series_downloaded=\"1\";" | series_db_execute
+}
+
 function series_id_set_submitted() {
     local instance="$1"
     local id="$2"
@@ -155,4 +173,40 @@ function series_id_set_complete() {
 
     echo "update series set series_completed=\"1\" where series_id=$id and series_instance=\"$instance\";" | series_db_execute
     return 0
+}
+
+function series_id_set_downloading() {
+    local instance="$1"
+    local id="$2"
+
+    if ! series_id_exists "$instance" "$id"; then
+        return 0
+    fi
+
+    echo "update series set series_downloaded=\"1\" where series_id=$id and series_instance=\"$instance\";" | series_db_execute
+}
+
+function series_id_set_downloaded() {
+    local instance="$1"
+    local id="$2"
+
+    if ! series_id_exists "$instance" "$id"; then
+        return 0
+    fi
+
+    # Just in case we 'race' with the resubmit.  It shouldn't happen.
+    series_id_set_submitted "$instance" "$id"
+    echo "update series set series_downloaded=\"2\" where series_id=$id and series_instance=\"$instance\";" | series_db_execute
+}
+
+function series_id_clear_downloaded() {
+    local instance="$1"
+    local id="$2"
+
+    if ! series_id_exists "$instance" "$id"; then
+        return 0
+    fi
+
+    series_id_clear_submitted "$instance" "$id"
+    echo "update series set series_downloaded=\"0\" where series_id=$id and series_instance=\"$instance\";" | series_db_execute
 }

pw_mon
@@ -127,5 +127,14 @@ function check_completed_series() {
     return 0
 }
 
+function check_undownloaded_series() {
+    get_undownloaded_jobs_as_line "$pw_instance" "$pw_project" | while IFS=\| read -r id url submitter_name submitter_email; do
+        echo "re-submitting series: $id"
+        series_id_clear_downloaded "$pw_instance" "$id"
+    done
+    return 0
+}
+
+check_undownloaded_series "$pw_instance" "$pw_project"
 check_completed_series "$pw_instance" "$pw_project"
 check_new_series "$pw_instance" "$pw_project"

series_downloaded
@@ -1,0 +1,65 @@
+#!/bin/bash
+# SPDX-Identifier: gpl-2.0-or-later
+# Copyright (C) 2019, Red Hat, Inc.
+#
+# Licensed under the terms of the GNU General Public License as published
+# by the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.  You may obtain a copy of the
+# license at
+#
+#    https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+source $(dirname $0)/series_db_lib.sh
+
+START="0"
+
+while [ "$1" != "" ]; do
+    if [ "$1" == "--start" ]; then
+        START="1"
+    elif [ "$1" == "--clear" ]; then
+        START="2"
+    else
+        break
+    fi
+    shift
+done
+
+if [ "$1" != "" ]; then
+    pw_instance="$1"
+    shift
+fi
+
+if [ "$1" != "" ]; then
+    series_id="$1"
+    shift
+fi
+
+if [ "X$pw_instance" == "X" -o "X$series_id" == "X" ]; then
+   echo "ERROR: Patchwork instance and series ID are unset."
+   echo "Please invoke as: $0 [--start] pw_instance series_id"
+   exit 1
+fi
+
+echo "ID:     $series_id"
+if ! series_id_exists "$pw_instance" "$series_id"; then
+    echo "ERROR: not found on instance $pw_instance"
+    exit 1
+fi
+
+
+if [ "$START" == "1" ]; then
+    echo "STATUS: starting"
+    series_id_set_downloading "$pw_instance" "$series_id"
+elif [ "$START" == "2" ]; then
+    echo "STATUS: clear"
+    series_id_clear_downloaded "$pw_instance" "$series_id"
+else
+    echo "STATUS: downloaded"
+    series_id_set_downloaded "$pw_instance" "$series_id"
+fi

commit a27074830180b3b5cdbc998129f430b4160ededb

series_db_lib: set downloaded to false
Commit e16b2ffea1d2 ("pw-ci: allow for downloads to fail") introduced the
download retry mechanism, but never set an initial value.  This caused
the partial series available mechanism never to select the series for
reconsideration.  Explicitly set this value to "0" and force the
incomplete series detection to consider series that weren't previously
available.

Fixes: e16b2ffea1d2 ("pw-ci: allow for downloads to fail")
Signed-off-by: Aaron Conole <aconole@redhat.com>
Acked-by: Michael Santana <msantana@redhat.com>
Reported-by: Ilya Maximets <i.maximets@ovn.org>
Reported-by: Michael Santana <msantana@redhat.com>

series_db_lib.sh
@@ -152,7 +152,7 @@ function series_db_add_false() {
     local submitter_email="$6"
     local completed="$7"
 
-    echo "insert into series(series_id, series_project, series_url, series_submitter, series_email, series_submitted, series_completed, series_instance) values (${id}, \"${project}\", \"${url}\", \"${submitter_name}\", \"${submitter_email}\", \"false\", \"${completed}\", \"${instance}\");" | series_db_execute
+    echo "insert into series(series_id, series_project, series_url, series_submitter, series_email, series_submitted, series_completed, series_instance, series_downloaded) values (${id}, \"${project}\", \"${url}\", \"${submitter_name}\", \"${submitter_email}\", \"false\", \"${completed}\", \"${instance}\", \"0\");" | series_db_execute
 }
 
 function series_id_exists() {